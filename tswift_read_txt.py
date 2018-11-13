eb80a1a0694e132acc9f43325845896570e15f25
Charlotte Mackin <cmackin@cshnyc.org>
2018-11-13 13:41:30 -0500

    tswift
    homework nov 13

--- tswift_read_txt.py
+++ tswift_read_txt.py
@@ -1,10 +1,18 @@
-count = 0
+# count = 0
+# with open("all_tswift_lyrics.txt") as tswift:
+#     for line in tswift:
+#         words = line.split()
+#         for x in words:
+#             if(x == "love"):
+#                 count = count+1
+#
+# print("The word love appeared: ")
+# print(count)
+
+from collections import Counter
 with open("all_tswift_lyrics.txt") as tswift:
     for line in tswift:
         words = line.split()
         for x in words:
-            if(x == "love"):
-                count = count+1
-
-print("The word love appeared: ")
-print(count)
+            print(x)
+            x = Counter(words)
